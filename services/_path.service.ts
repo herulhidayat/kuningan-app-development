a8dbb816060ec675c56b38dc59c14fdc2ed2af0b
Khairul Hidayat <khairul.hidayat@jkt1.ebdesk.com>
2025-03-26 20:02:06 +0700

    [TASK] change proxy config and path services

--- services/_path.service.ts
+++ services/_path.service.ts
@@ -1,8 +1,8 @@
 export const API_PATH = () => {
     return {
       auth: {
-        login: "api/v1/management/public/auth/login",
-        refresh: "api/v1/management/public/auth/refresh",
+        login: "api/v1/public/auth/login",
+        refresh: "api/v1/public/auth/refresh",
       },
     }
 }
